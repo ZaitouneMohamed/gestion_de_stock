42926c7cc485cb8c94b7ca08181fffa243a02422
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-10 19:30:46 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::permanentRedirect('/', '/admin');
 
 
-Route::prefix("admin")->middleware("auth")->group(function(){
+Route::prefix("admin")->middleware("auth")->group(function () {
     Route::view('/', 'admin.index');
 });
 // 0710750199
 
 Route::view('/login', 'auth.login')->name("login");
 
-Route::controller(AuthController::class)->name("auth.")->group(function(){
-    Route::post("login","login")->name("login");
-    Route::get("logout","logout")->name("logout");
+Route::controller(AuthController::class)->name("auth.")->group(function () {
+    Route::post("login", "login")->name("login");
+    Route::get("logout", "logout")->name("logout");
+});
+
+Route::get('/mail', function () {
+    $recipient = 'dwm23-zaitoune@ifiag.com';
+    $subject = 'Demo Email';
+    $content = 'Hello there! This is a demo email sent from Laravel.';
+
+    Mail::raw($content, function ($message) use ($recipient, $subject) {
+        $message->to($recipient);
+        $message->subject($subject);
+    });
 });
