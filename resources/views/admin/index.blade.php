adcf6fd92ea05b336f8fa7e7b81b90f845dc6aee
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-10 17:03:58 +0100

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -18,10 +18,70 @@
     </div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        
+                        <div class="d-md-flex align-items-center">
+                            <div>
+                                <h4 class="card-title">Site Analysis</h4>
+                                <h5 class="card-subtitle">Overview of Latest Month</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!-- column -->
+                            <div class="col-lg-9">
+                                <div class="flot-chart">
+                                    <div class="flot-chart-content" id="flot-line-chart"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fa-brands fa-product-hunt"></i>
+                                            <h5 class="m-b-0 m-t-5">{{ \App\Models\Product::count() }}</h5>
+                                            <small class="font-light">Total produit</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fa-solid fa-sitemap"></i>
+                                            <h5 class="m-b-0 m-t-5">{{ \App\Models\Categorie::count() }}</h5>
+                                            <small class="font-light">Categories</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 m-t-15">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fas fa-arrow-alt-circle-down"></i>
+                                            <h5 class="m-b-0 m-t-5">{{ \App\Models\Entreé::count() }}</h5>
+                                            <small class="font-light">Entreé</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 m-t-15">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fas fa-arrow-alt-circle-up"></i>
+                                            <h5 class="m-b-0 m-t-5">{{ \App\Models\Sortieé::count() }}</h5>
+                                            <small class="font-light">Sortie</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 m-t-15">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fa fa-table m-b-5 font-16"></i>
+                                            <h5 class="m-b-0 m-t-5">{{ \App\Models\Uniteé::count() }}</h5>
+                                            <small class="font-light">Uniteé</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 m-t-15">
+                                        <div class="bg-dark p-10 text-white text-center">
+                                            <i class="fa fa-globe m-b-5 font-16"></i>
+                                            <h5 class="m-b-0 m-t-5">8540</h5>
+                                            <small class="font-light">Online Orders</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- column -->
+                        </div>
                     </div>
                 </div>
             </div>
