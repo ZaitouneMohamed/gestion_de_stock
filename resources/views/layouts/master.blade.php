ada29ba27edb0445009a444b2bd511b1d1b4b8cd
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-12 16:08:00 +0100

    add bootstrap cdn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,7 @@
     <!-- Custom CSS -->
     @yield("style")
     @livewireStyles
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="{{ asset('dashboard/dist/css/style.min.css') }}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -331,6 +332,7 @@
     <!--Menu sidebar -->
     <script src="{{ asset('dashboard/dist/js/sidebarmenu.js') }}"></script>
     <!--Custom JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="{{ asset('dashboard/dist/js/custom.min.js') }}"></script>
 </body>
 
