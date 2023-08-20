88319ecaba80061daf8189bfebdd8d157479c6bf
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-20 22:29:06 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\History;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,6 +11,13 @@ class HomeController extends Controller
     function HistoryList()
     {
         $history = History::latest()->get();
-        return view('admin.history',compact("history"));
+        return view('admin.history', compact("history"));
+    }
+
+    function GetProductOfCategorie(Request $request)
+    {
+        $id = $request->categorie_id;
+        $products = Product::where('categorie_id', $id)->get();
+        return response()->json($products);
     }
 }
