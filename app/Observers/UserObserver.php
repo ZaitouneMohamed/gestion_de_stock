478bb890fd350818975d763b34c5cc2c7a9cdfca
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-10-09 13:12:47 +0100

    Create UserObserver.php

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\History;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        $description = "new sortie by : " . $user->name . " - sortie " . $entree->qte . " of " . $product->name;
+        $new_history = new History(["description" => $description]);
+        $product->History()->save($new_history);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
